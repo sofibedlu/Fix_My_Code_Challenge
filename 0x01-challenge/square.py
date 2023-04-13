bd4ca53979a6218a98f9e0f043a678f4cc2537d2
sofi <bedlusofi@gmail.com>
2023-04-13 14:23:55 +0000

    update area

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -13,11 +13,11 @@ class Square():
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.width * self.height
 
     def permiter_of_my_Square(self):
         """ permiter of a square"""
-        return (self.width * 4)
+        return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
         """ string representation of the instances"""
