1ee44260a3e817f59c4255032f00717afb86ff7d
sofi <bedlusofi@gmail.com>
2023-04-13 14:12:21 +0000

    square updated

---
+++ 0x01-challenge/square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""define Square instances"""
+
+class Square():
+    
+    width = 0
+    height = 0
+
+    
+    def __init__(self, *args, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def area_of_my_square(self):
+        """ Area of the square """
+        return self.width * self.width
+
+    def PermiterOfMySquare(self):
+        """ permiter of a square""" 
+        return (self.width * 4)
+
+    def __str__(self):
+        """ string representation of the instances"""
+        return "{}/{}".format(self.width, self.height)
+
+if __name__ == "__main__":
+
+    s = Square(width=12, height=12)
+    print(s)
+    print(s.area_of_my_square())
+    print(s.PermiterOfMySquare())
